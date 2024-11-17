7942f69f2f8d00f9964fff311d5e19ebb6be49b5
SatpalSinghHere <satpalsinghalive1@gmail.com>
2024-11-17 20:15:22 +0530

    "Updated SocketService to enable CORS, added SocketProvider to layout, refactored page styles and functionality to include chat input and send button, and updated dependencies in package.json and package-lock.json."

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -4,11 +4,17 @@ class SocketService {
     private _io: Server
     constructor(){
         console.log("Init Socket Service ...")
-        this._io = new Server()
+        this._io = new Server({
+            cors: {
+                origin: '*',
+                allowedHeaders: ['*']
+            }
+        })
     }
 
     public initListeners (){
         const io = this._io
+        console.log("Init Socket listeners...")
         io.on('connect', (socket)=> {
             console.log('new socket connected ', socket.id)
 
